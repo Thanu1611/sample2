0310bf308f3f816b6a210faa54bffde100c501be
Thedsanamoorthysarma Thanushika <121013173+Thanu1611@users.noreply.github.com>
2023-01-03 14:33:09 +0530

    saanu oru paithiyam

--- sum.java
+++ sum.java
@@ -3,9 +3,10 @@ class sum
     public static void main(String args[])
     {
         int a,b;
+        
         a=3;
         b=4;
-        int c=8;
+        int c=8;  
         int d=2;
         System.out.println(a+b);//summation
     }
